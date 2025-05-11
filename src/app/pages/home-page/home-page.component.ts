450f203d6dae372b095bffa1194fd4c1b0b2064e
Justin Lopez <justinlopez588@gmail.com>
2025-05-11 18:00:08 -0500

    Home page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { DpDatePickerModule } from 'ng2-date-picker';
 
 @Component({
-  imports: [],
+  imports: [DpDatePickerModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+
+  datePickerConfig = {
+    format: 'YYYY-MM-DD',
+    minYear: 2000,
+    maxYear: 2030,
+    showWeekNumbers: false,
+    closeOnDateSelection: true,
+    dateInputFormat: 'YYYY-MM-DD',
+    displayFormat: 'YYYY-MM-DD',
+    showDropdowns: true,
+    showTodayButton: true,
+    todayButtonLabel: 'Today',
+  }
 
 }
