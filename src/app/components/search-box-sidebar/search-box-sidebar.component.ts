abb1ba84caaa4760cdc3dcd791c16b9df25bc974
Justin Lopez <justinlopez588@gmail.com>
2025-05-17 15:51:02 -0500

    images added

---
+++ src/app/components/search-box-sidebar/search-box-sidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-box-sidebar',
+  imports: [],
+  templateUrl: './search-box-sidebar.component.html',
+  styles: ``
+})
+export class SearchBoxSidebarComponent {
+
+}
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { DpDatePickerModule } from 'ng2-date-picker';
+import { HotelListComponent } from "../../components/hotel-list/hotel-list.component";
+import { SearchBoxSidebarComponent } from "../../components/search-box-sidebar/search-box-sidebar.component";
 
 @Component({
-  imports: [DpDatePickerModule],
+  imports: [DpDatePickerModule, HotelListComponent, SearchBoxSidebarComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
